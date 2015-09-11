b139de68100a5203c791fabf89bd429c91f6d04d
SimonBirrell <simon@eurodemand.com>
2015-09-11 09:31:53 +0100

    Web listener for Heroku

--- server.js
+++ server.js
@@ -22,6 +22,27 @@ exports.launch = function() {
     browserServer = new WebSocketServer({ port: 8001 });
     browserServer.on('connection', handleBrowserConnection.handleBrowserConnection);
     serverLog('Ready to handle browser connections...');
+
+    // All the below is simply to get the server running on Heroku (which require a web port)
+    //Lets require/import the HTTP module
+    var http = require('http');
+
+    //Lets define a port we want to listen to
+    const PORT=process.env.PORT || 8080; 
+
+    //We need a function which handles requests and send response
+    function handleRequest(request, response){
+        response.end('It Works!! Path Hit: ' + request.url);
+    }
+
+    //Create a server
+    var server = http.createServer(handleRequest);
+
+    //Lets start our server
+    server.listen(PORT, function(){
+        //Callback triggered when server is successfully listening. Hurray!
+        console.log("Server listening on: http://localhost:%s", PORT);
+    });
         
     return agentServer;    
 }
