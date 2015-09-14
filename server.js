9e2da5777d5eba7affabcf3558a0efb3463e5d73
SimonBirrell <simon@eurodemand.com>
2015-09-14 09:12:09 +0100

    Tests pass

--- server.js
+++ server.js
@@ -38,12 +38,13 @@ exports.launch = function() {
     //Create a server
     var server = http.createServer(handleRequest);
 
+/*
     //Lets start our server
     server.listen(PORT, function(){
         //Callback triggered when server is successfully listening. Hurray!
         console.log("Server listening on: http://localhost:%s", PORT);
     });
-        
+*/
     return agentServer;    
 }
 
